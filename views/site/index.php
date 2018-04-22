f408872e1a806a1df8299b7efba382e84eb086ac
Artur Avdonin <NoName009.aa@gmail.com>
2018-04-22 14:44:51 +0500

    FIX: Не верная директория view

--- views/site/index.php
+++ views/site/index.php
@@ -20,8 +20,7 @@ $this->title = 'My Yii Application';
                     <h2 align="center">Рабочее меню</h2>
                         <p><?=Html::a('Перейти в редактор формата',['/admin/format-list/index'],['class'=>'btn btn-success btn-sm btn-block', 'style'=>'padding-top: 16px; padding-bottom: 16px; font-size: 17px; margin-top: 30px; margin-bottom: 30px']);?></p>
                         <p><?=Html::a('Список заявок на раскрой',['/admin/order-item/index'],['class'=>'btn btn-success btn-sm btn-block', 'style'=>'padding-top: 16px; padding-bottom: 16px; font-size: 17px; margin-top: 30px; margin-bottom: 30px;']);?></p>            
-                        <p><?=Html::a('Создать заявку на раскрой',['/admin/order/create'],['class'=>'btn btn-success btn-sm btn-block', 'style'=>'padding-top: 16px; padding-bottom: 16px; font-size: 17px; margin-top: 30px; margin-bottom: 30px;']);?></p> 
-                        <p><?=Html::a('Удалить выбранную заявку',['/admin/order/create'],['class'=>'btn btn-success btn-sm btn-block','style'=>'padding-top: 16px; padding-bottom: 16px; font-size: 17px; margin-top: 30px; margin-bottom: 30px;']);?></p>      
+                        <p><?=Html::a('Создать заявку на раскрой',['/admin/order/create'],['class'=>'btn btn-success btn-sm btn-block', 'style'=>'padding-top: 16px; padding-bottom: 16px; font-size: 17px; margin-top: 30px; margin-bottom: 30px;']);?></p>
                 </div>
                 <div class="col-lg-9">
                     <h2 align="center">Список заявок</h2>
